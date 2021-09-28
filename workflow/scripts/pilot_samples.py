bf811b5f6a718cffe5ae31758a287fe4b37c404f
Bhavya Singh <singhb39@mcmaster.ca>
2021-09-28 16:46:59 -0400

    added filtered dict for pilot

--- workflow/scripts/pilot_samples.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-from collections import Counter
-from collections import OrderedDict
-import pandas as pd
-
-samples_tsv = 'config/samples.tsv'
-read_samples_tsv = pd.read_csv(samples_tsv, sep='\t')
